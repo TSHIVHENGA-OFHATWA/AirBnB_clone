fef3681a7d6a8eb3ddf9185c6fc230a096534ee0
OFHATWA <tofhatwa@gmail.com>
2024-05-19 15:02:31 +0200

    Updating the base model to generate a dictinary representation of instance

--- models/base_model.py
+++ models/base_model.py
@@ -5,16 +5,6 @@
 
 Classes:
     BaseModel
-
-Example:
-    instance = BaseModel()
-    print(instance)
-    # [BaseModel] (<id>) {'id': <id>, 'created_at':
-        <datetime>, 'updated_at': <datetime>}
-    instance.save()
-    print(instance.to_dict())
-    # {'id': <id>, 'created_at': <iso_datetime>, 'updated_at':
-        <iso_datetime>, '__class__': 'BaseModel'}
 '''
 
 
@@ -31,14 +21,24 @@ class BaseModel:
         updated_at (datetime): The datetime for the instance was last updated.
     """
 
-    def __init__(self):
-        """Initializes a new instance of BaseModel.
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance.
 
-        Sets the id, created_at, and uodated_at attributes.
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance.
